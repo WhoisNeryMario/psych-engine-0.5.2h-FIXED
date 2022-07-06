6ece70167580611cd749c72a446b2c7bc96f1c0f
WhoisNeryMario <108764251+WhoisNeryMario@users.noreply.github.com>
2022-07-06 10:06:44 -0700

    Add files via upload

---
+++ assets/preload/data/baana/script.lua
@@ -0,0 +1,5 @@
+function onCreate()
+makeLuaText('credits', 'SONG, CHARACTER MADE BY  cadencraft0622                     Baldi TTS is used from Uberduck', 350, 20, 630)
+setTextAlignment('credits','left')
+addLuaText('credits')
+end
