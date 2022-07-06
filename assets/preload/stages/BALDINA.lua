be3818be03391d5396861c7d3eafa58b1f037ea8
WhoisNeryMario <108764251+WhoisNeryMario@users.noreply.github.com>
2022-07-06 08:12:20 -0700

    Add files via upload

---
+++ assets/preload/stages/BALDINA.lua
@@ -0,0 +1,7 @@
+function onCreate()
+	makeLuaSprite('school', 'BALDINAs', -570, -290);
+	scaleObject('school', 2, 2);
+	setScrollFactor('school', 1.1, 1.2);
+
+	addLuaSprite('school', false);
+end
---
+++ assets/preload/stages/schoolhouse.lua
@@ -0,0 +1,7 @@
+function onCreate()
+	makeLuaSprite('school', 'schoolhouse', -570, -290);
+	scaleObject('school', 2, 2);
+	setScrollFactor('school', 1.1, 1.2);
+
+	addLuaSprite('school', false);
+end
